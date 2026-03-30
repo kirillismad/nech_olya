e7554d0c8d672dd338130713056c37189b869ffa
Belka <necholya1@gmail.com>
2026-03-30 19:16:53 +0300

    valid palindrome

---
+++ leetcode/valid_palindrome.go
@@ -0,0 +1,29 @@
+package main
+
+import "unicode"
+
+func validPalindrome(s string) bool {
+	str := cleanStrings(s)
+	if str == "" {
+		return true
+	}
+
+	for i := 0; i < len(str); i++ {
+		for j := len(str) - 1; j > 0; j-- {
+			if str[i] == str[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func cleanStrings(s string) string {
+	var result []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			result = append(result, unicode.ToLower(r))
+		}
+	}
+	return string(result)
+}
