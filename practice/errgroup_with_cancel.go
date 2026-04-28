a1d540178363fa45046a377505505739232c8cd9
Belka <necholya1@gmail.com>
2026-04-28 07:27:22 +0300

    fix

--- practice/errgroup_with_cancel.go
+++ practice/errgroup_with_cancel.go
@@ -14,29 +14,28 @@ func ErrGroupWithCancel() {
 
 	g.Go(func() error {
 		log.Println("1 горутина начала работу")
-		for v := range 10 {
-			log.Printf("Итерация %d в горутине 1\n", v+1)
-			if err := ctx.Err(); err != nil {
-				log.Printf("Цикл остановлен на итерации %d: %v\n", v+1, err)
-				return err
-			}
-			time.Sleep(50 * time.Millisecond)
+		if err := ctx.Err(); err != nil {
+			log.Printf("Горутина 1 остановлена: %v\n", err)
+			return err
 		}
+		time.Sleep(500 * time.Millisecond)
 		return fmt.Errorf("горутина 1 вернула ошибку")
 	})
 	for i := 2; i < 5; i++ {
 		g.Go(func() error {
 			log.Printf("%d горутина начала работу\n", i)
-			for v := range 10 {
+			select {
+			case <-ctx.Done():
+				log.Printf("Горутина %d остановлена, время остановки: %v\n", i, time.Now())
 				if err := ctx.Err(); err != nil {
-					log.Printf("Горутина %d остановлен на итерации %d: %v, время остановки: %v\n", i, v+1, err, time.Now())
 					return err
 				}
-				time.Sleep(50 * time.Millisecond)
+			case <-time.After(100 * time.Millisecond):
+
 			}
 			return nil
 		})
 	}
 	err := g.Wait()
-	fmt.Println("Возвращенная ошибка:", err)
+	log.Println("Возвращенная ошибка:", err)
 }
