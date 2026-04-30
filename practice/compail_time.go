a23de86f76e615eae0bbb2bd29d8e5d90275de3c
Belka <necholya1@gmail.com>
2026-04-30 06:29:00 +0300

    feature/task-164

---
+++ practice/compail_time.go
@@ -0,0 +1,49 @@
+package practice
+
+import "fmt"
+
+type Describer interface {
+	Describe() string
+}
+
+type User struct {
+	Name string
+}
+
+func (u *User) Describe() string {
+	return u.Name
+}
+
+type Order struct {
+	Address string
+}
+
+func (o *Order) Describe() string {
+	return o.Address
+}
+
+type Product struct {
+	Name string
+}
+
+func (p *Product) Describe() string {
+	return p.Name
+}
+
+var _ Describer = (*User)(nil)
+var _ Describer = (*Order)(nil)
+var _ Describer = (*Product)(nil)
+
+func PrintAll(items []Describer) {
+	for _, item := range items {
+		fmt.Println(item.Describe())
+	}
+}
+
+func compailTime() {
+	items := []Describer{
+		&User{Name: "Olya"},
+		&Order{Address: "Lenin street"},
+		&Product{Name: "Bread"}}
+	PrintAll(items)
+}
