bb3e186db6bf49793f5c6b829510f82c949589e0
Kirill Pech <pechkirill@gmail.com>
2026-05-12 18:02:36 +0300

    done

--- practice/cmd/task184/main.go
+++ practice/cmd/task184/main.go
@@ -8,21 +8,30 @@ import (
 
 func main() {
 	data := []byte("Hello, файл!\n")
-	err := os.WriteFile("hello_file.txt", data, 0644)
+	err := os.WriteFile("hello_file.txt", data, 0644) // запись в файл
 	if err != nil {
 		fmt.Println("failed to write to file")
 		return
 	}
-	file, err := os.OpenFile("hello_file.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("failed to open file")
-	}
-	defer file.Close()
-	w := bufio.NewWriter(file)
-	w.WriteString("Good morning!\n")
-	w.Flush()
-	w.WriteString("Good night!\n")
-	w.Flush()
+	func() {
+		file, err := os.OpenFile("hello_file.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("failed to open file")
+		}
+		defer file.Close()
+
+		w := bufio.NewWriter(file)
+		defer w.Flush()
+
+		_, err = w.WriteString("Good morning!\n") // запись в память
+		if err != nil {
+			fmt.Println("failed to write to buffer")
+		}
+		_, err = w.WriteString("Good night!\n") // запись в память
+		if err != nil {
+			fmt.Println("failed to write to buffer")
+		}
+	}()
 
 	data, err = os.ReadFile("hello_file.txt")
 	if err != nil {
