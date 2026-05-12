8fbf1da6c132ee797e42f1c7c239294a63231235
Kirill Pech <pechkirill@gmail.com>
2026-05-12 17:44:16 +0300

    done

--- practice/err_custom_type_test.go
+++ practice/err_custom_type_test.go
@@ -11,19 +11,18 @@ func TestCustomType(t *testing.T) {
 			t.Error("data entered correctly")
 			return
 		}
-		if err != nil {
-			if val, ok := err.(*ValidationError); ok {
-				if name == "" {
-					t.Logf("Field: %s\n Message: %s", val.Field, val.Message)
-				} else if age < 0 {
-					t.Errorf("Field: %s\n Message: %s", val.Field, val.Message)
-					return
-				} else {
-					t.Errorf("another error")
-					return
-				}
-
-			}
+		validationErr, ok := err.(*ValidationError)
+		if !ok {
+			t.Errorf("unexpected error type: %T", err)
+			return
+		}
+		if validationErr.Field != "name" {
+			t.Errorf("unexpected field: %s", validationErr.Field)
+			return
+		}
+		if validationErr.Message != "cannot be empty" {
+			t.Errorf("unexpected message: %s", validationErr.Message)
+			return
 		}
 	})
 	t.Run("tc 2:", func(t *testing.T) {
@@ -31,76 +30,55 @@ func TestCustomType(t *testing.T) {
 		age := 25
 		err := ValidateUser(name, age)
 		if err != nil {
-			if val, ok := err.(*ValidationError); ok {
-				if name == "" {
-					t.Errorf("Field: %s\n Message: %s", val.Field, val.Message)
-					return
-				} else if age < 0 {
-					t.Errorf("Field: %s\n Message: %s", val.Field, val.Message)
-					return
-				}
-
-				t.Errorf("another error")
-				return
-
-			}
-			if err == nil {
-				t.Log("data entered correctly")
-			}
+			t.Errorf("unexpected error: %v", err)
+			return
 		}
 	})
-	t.Run("tc 3:", func(t *testing.T) {
+	t.Run("tc 3: error: negative age", func(t *testing.T) {
 		name := "Alice"
 		age := -10
 		err := ValidateUser(name, age)
-		if err != nil {
-			if val, ok := err.(*ValidationError); ok {
-				if name == "" {
-					t.Errorf("Field: %s\n Message: %s", val.Field, val.Message)
-				} else if age < 0 {
-					t.Logf("Field: %s\n Message: %s", val.Field, val.Message)
-					return
-				} else {
-					t.Errorf("another error")
-					return
-				}
-			}
-			if err == nil {
-				t.Error("data entered correctly")
-				return
-			}
+		if err == nil {
+			t.Error("data entered correctly")
+			return
+		}
+		validationErr, ok := err.(*ValidationError)
+		if !ok {
+			t.Errorf("unexpected error type: %T", err)
+			return
+		}
+		if validationErr.Field != "age" {
+			t.Errorf("unexpected field: %s", validationErr.Field)
+			return
+		}
+		if validationErr.Message != "cannot be negative" {
+			t.Errorf("unexpected message: %s", validationErr.Message)
+			return
 		}
 	})
 
-	t.Run("tc 4:", func(t *testing.T) {
+	t.Run("tc 4: success", func(t *testing.T) {
 		id := 0
 		err := FindUserByID(id)
 		if err != nil {
-			if val, ok := err.(*NotFoundError); ok {
-				t.Errorf("ID: %d", val.ID)
-				return
-			} else {
-				t.Errorf("another error")
-				return
-			}
-		}
-		if err == nil {
-			t.Log("data entered correctly")
+			t.Errorf("unexpected error: %v", err)
+			return
 		}
 	})
-	t.Run("tc 5:", func(t *testing.T) {
+	t.Run("tc 5: error: user not found", func(t *testing.T) {
 		id := 100
 		err := FindUserByID(id)
-		if err != nil {
-			if val, ok := err.(*NotFoundError); ok {
-				t.Logf("Error ID: %d", val.ID)
-			} else {
-				t.Errorf("another error")
-				return
-			}
-		}
 		if err == nil {
-			t.Error("data entered correctly")
+			t.Error("user found")
+			return
+		}
+		notFoundErr, ok := err.(*NotFoundError)
+		if !ok {
+			t.Errorf("unexpected error type: %T", err)
+			return
+		}
+		if notFoundErr.ID != id {
+			t.Errorf("unexpected ID: %d", notFoundErr.ID)
 			return
 		}
 	})
