d57a59ebfedfbf72e52ca46a8c2aa57ad150e35b
Belka <necholya1@gmail.com>
2026-05-13 17:48:23 +0300

    fix

--- practice/errors_join_test.go
+++ practice/errors_join_test.go
@@ -6,44 +6,42 @@ import (
 )
 
 func TestErrorJoin(t *testing.T) {
-	t.Run("tc 1", func(t *testing.T) {
+	t.Run("tc 1: error no name, qty zero, negative price", func(t *testing.T) {
 		var name string
 		var qty int
 		price := -1.1
 		err := ValidateOrder(name, qty, price)
+		if err == nil {
+			t.Error("error expected")
+			return
+		}
 		if err != nil {
-			if errors.Is(err, ErrName) && errors.Is(err, ErrQty) && errors.Is(err, ErrPrice) {
-				t.Logf("err== %v, ", err)
-			} else {
+			if !errors.Is(err, ErrName) && !errors.Is(err, ErrQty) && !errors.Is(err, ErrPrice) {
 				t.Errorf("unknown error:%v ", err)
 				return
 			}
-		}
-		if err == nil {
-			t.Error("ошибок нет")
-			return
+			t.Logf("err== %v, ", err)
 		}
 	})
-	t.Run("tc 2", func(t *testing.T) {
+	t.Run("tc 2: error no name", func(t *testing.T) {
 		var name string
 		qty := 5
 		price := 100.2
 		err := ValidateOrder(name, qty, price)
-		if err != nil {
-			if errors.Is(err, ErrName) {
-				t.Logf("ErrName== %s", err)
-			} else {
-				t.Errorf("unknown error: %v", err)
-				return
-			}
-		}
 		if err == nil {
 			t.Error("ошибок нет")
 			return
 		}
+		if err != nil {
+			if !errors.Is(err, ErrName) {
+				t.Errorf("unknown error: %v", err)
+				return
+			}
+			t.Logf("ErrName== %s", err)
+		}
 	})
 
-	t.Run("tc 3", func(t *testing.T) {
+	t.Run("tc 3: success", func(t *testing.T) {
 		name := "milk"
 		qty := 5
 		price := 100.2
@@ -52,9 +50,6 @@ func TestErrorJoin(t *testing.T) {
 			t.Errorf("this result is not expected, error, %v", err)
 			return
 		}
-		if err == nil {
-			t.Log("no errors")
-			return
-		}
+		t.Log("no errors")
 	})
 }
