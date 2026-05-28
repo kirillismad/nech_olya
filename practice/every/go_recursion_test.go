9718ba306be09daef114dae63f7b37b04eaf10a7
Belka <necholya1@gmail.com>
2026-05-28 07:49:19 +0300

    feature/task-232

---
+++ practice/every/go_recursion_test.go
@@ -0,0 +1,47 @@
+package every
+
+import (
+	"testing"
+)
+
+func TestRecursion(t *testing.T) {
+	testsSum := []struct {
+		n        int
+		expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{5, 15},
+		{7, 28},
+	}
+	for _, test := range testsSum {
+		result := SumToN(test.n)
+		if result != test.expected {
+			t.Fatalf(
+				"expected %d, got %d",
+				test.expected,
+				result,
+			)
+		}
+	}
+
+	testsFactorial := []struct {
+		n        int
+		expected int
+	}{
+		{0, 1},
+		{1, 1},
+		{5, 120},
+		{7, 5040},
+	}
+	for _, test := range testsFactorial {
+		result := Factorial(test.n)
+		if result != test.expected {
+			t.Fatalf(
+				"expected %d, got %d",
+				test.expected,
+				result,
+			)
+		}
+	}
+}
