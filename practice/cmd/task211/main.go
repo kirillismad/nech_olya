94aa72fd38106f89db893c91816e4e90963dbe51
Belka <necholya1@gmail.com>
2026-06-07 19:32:54 +0300

    fix

--- practice/cmd/task211/main.go
+++ practice/cmd/task211/main.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 )
 
@@ -41,11 +44,12 @@ type GetPostQuery struct {
 }
 
 type CreatePostCommand struct {
-	Title string `json:"title"`
-	Body  string `json:"body"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	UserID int    `json:"userID"`
 }
 
-func (api APIClient) ListPost(ctx context.Context, query ListPostQuery) ([]Post, error) {
+func (api APIClient) ListPost(ctx context.Context, query ListPostQuery, num int) ([]Post, error) {
 	if query.Page < 1 {
 
 		return nil, fmt.Errorf("page must be greater than 0")
@@ -70,7 +74,7 @@ func (api APIClient) ListPost(ctx context.Context, query ListPostQuery) ([]Post,
 		return nil, err
 	}
 
-	resp, err := api.Retry(req)
+	resp, err := api.retry(req, num)
 	if err != nil {
 		return nil, err
 	}
@@ -92,7 +96,7 @@ func (api APIClient) ListPost(ctx context.Context, query ListPostQuery) ([]Post,
 
 }
 
-func (api APIClient) GetPost(ctx context.Context, query GetPostQuery) (Post, error) {
+func (api APIClient) GetPost(ctx context.Context, query GetPostQuery, num int) (Post, error) {
 	u, err := url.Parse(api.BaseURL)
 	if err != nil {
 		return Post{}, err
@@ -104,7 +108,7 @@ func (api APIClient) GetPost(ctx context.Context, query GetPostQuery) (Post, err
 		return Post{}, err
 	}
 
-	resp, err := api.Retry(req)
+	resp, err := api.retry(req, num)
 	if err != nil {
 		return Post{}, err
 	}
@@ -126,7 +130,7 @@ func (api APIClient) GetPost(ctx context.Context, query GetPostQuery) (Post, err
 
 }
 
-func (api APIClient) CreatePost(ctx context.Context, command CreatePostCommand) (Post, error) {
+func (api APIClient) CreatePost(ctx context.Context, command CreatePostCommand, num int) (Post, error) {
 
 	u, err := url.Parse(api.BaseURL)
 	if err != nil {
@@ -139,6 +143,9 @@ func (api APIClient) CreatePost(ctx context.Context, command CreatePostCommand) 
 		return Post{}, err
 	}
 
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u.String(), bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return Post{}, err
@@ -146,7 +153,7 @@ func (api APIClient) CreatePost(ctx context.Context, command CreatePostCommand) 
 
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := api.Retry(req)
+	resp, err := api.retry(req, num)
 	if err != nil {
 		return Post{}, err
 	}
@@ -167,14 +174,26 @@ func (api APIClient) CreatePost(ctx context.Context, command CreatePostCommand) 
 
 }
 
-func (api APIClient) Retry(req *http.Request) (*http.Response, error) {
+func (api APIClient) retry(req *http.Request, num int) (*http.Response, error) {
 	var err error
 	var resp *http.Response
+	if num == 0 {
+		num = 3
+	}
 
-	for attempt := 1; attempt <= 3; attempt++ {
+	for attempt := 1; attempt <= num; attempt++ {
 		resp, err = api.Client.Do(req)
 		if err != nil {
 			fmt.Println("network error:", err)
+			continue
+		}
+		if resp.StatusCode >= 500 {
+			fmt.Println("invalid status code:", resp.StatusCode)
+			err = fmt.Errorf("invalid status code: %d", resp.StatusCode)
+			continue
+		}
+		if errors.Is(err, context.DeadlineExceeded) {
+			fmt.Println("request timeout:", err)
 			continue
 		}
 		return resp, nil
@@ -183,6 +202,25 @@ func (api APIClient) Retry(req *http.Request) (*http.Response, error) {
 }
 
 func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		fmt.Println("request canceled")
+		os.Exit(1)
+	}()
+
+	strValue := os.Getenv("RETRY_COUNT")
+	if strValue == "" {
+		strValue = "0"
+	}
+
+	num, err := strconv.Atoi(strValue)
+	if err != nil {
+		fmt.Println("type conversion error: ", err)
+		return
+	}
 	if len(os.Args) < 2 {
 		fmt.Println("command required")
 		return
@@ -219,7 +257,7 @@ func main() {
 		posts, err := api.ListPost(ctx, ListPostQuery{
 			Page:     page,
 			PageSize: pageSize,
-		})
+		}, num)
 
 		if err != nil {
 			fmt.Println("list error:", err)
@@ -238,7 +276,7 @@ func main() {
 		}
 		post, err := api.GetPost(ctx, GetPostQuery{
 			ID: os.Args[2],
-		})
+		}, num)
 		if err != nil {
 			fmt.Println("get error:", err)
 			return
@@ -250,23 +288,30 @@ func main() {
 			post.Body,
 		)
 	case "create":
-		if len(os.Args) < 4 {
+		if len(os.Args) < 5 {
 
 			fmt.Println("title and body required")
 			return
 		}
+		id, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 		post, err := api.CreatePost(ctx, CreatePostCommand{
-			Title: os.Args[2],
-			Body:  os.Args[3],
-		})
+			Title:  os.Args[2],
+			Body:   os.Args[3],
+			UserID: id,
+		}, num)
 		if err != nil {
 			fmt.Println("create error:", err)
 			return
 		}
-		fmt.Printf("Created post:\nID: %d\nTitle: %s\nBody: %s\n",
+		fmt.Printf("Created post:\nID: %d\nTitle: %s\nBody: %s\nUserID: %d\n",
 			post.ID,
 			post.Title,
 			post.Body,
+			post.UserID,
 		)
 	default:
 		fmt.Println("unknown command:", command)
